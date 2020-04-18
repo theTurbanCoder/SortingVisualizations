bec621ac917ebb6d4e2afbf6ccbf5a56f89cccea
Tanveer Anand <tanand1@hawk.iit.edu>
2020-04-18 02:39:47 -0700

    testhash router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import {HashRouter, Switch, Route} from 'react-router-dom'
 import './App.css';
 import SortingVisualizer from './components/SortingVisualizer';
 
 function App() {
   return (
     <div className="App">
-     <SortingVisualizer />
+      <HashRouter>
+        <Switch>
+            <Route exact path='/' component={SortingVisualizer} />
+        </Switch>
+      </HashRouter>
+     
     </div>
   );
 }
