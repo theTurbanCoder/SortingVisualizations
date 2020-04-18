161a546df3da51f8da83268be6767e8f75c8e733
theTurbanCoder <tanand1@hawk.iit.edu>
2020-04-18 09:06:33 +0000

    Updates

---
+++ precache-manifest.a590a80eef4a4d5f3ab95ce8271cf537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0cf70285cc96b5ed836300654a6408",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d77c1449a82f69a5227",
+    "url": "/static/css/main.13538e3f.chunk.css"
+  },
+  {
+    "revision": "0338dade397ca7209738",
+    "url": "/static/js/2.7a6cc589.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a6cc589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d77c1449a82f69a5227",
+    "url": "/static/js/main.3e55f3be.chunk.js"
+  },
+  {
+    "revision": "116a200dd9448d7dcc3f",
+    "url": "/static/js/runtime-main.3f2e1f9c.js"
+  }
+]);
